b4bccb9001dca2127f47954c1ece03a35d888943
Mateus Vagner <mateus.vagner@gmail.com>
2022-10-09 14:52:53 -0300

    refactor: change resource name

---
+++ src/main/java/com/foliapp/userservice/resource/UserResource.java
@@ -0,0 +1,43 @@
+package com.foliapp.userservice.resource;
+
+import java.util.List;
+
+public class UserResource {
+    String name;
+    String email;
+    String password;
+    List<String> roles;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+}
