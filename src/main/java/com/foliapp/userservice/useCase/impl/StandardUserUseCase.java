6319cff48ac737565f99b46fcfbbf4103c9e7ab3
Mateus Vagner <mateus.vagner@gmail.com>
2022-11-05 14:44:45 -0300

    refactor: add role to new user

--- src/main/java/com/foliapp/userservice/useCase/impl/StandardUserUseCase.java
+++ src/main/java/com/foliapp/userservice/useCase/impl/StandardUserUseCase.java
@@ -1,10 +1,12 @@
 package com.foliapp.userservice.useCase.impl;
 
+import com.foliapp.userservice.domain.Role;
 import com.foliapp.userservice.domain.User;
 import com.foliapp.userservice.exception.NameLengthNotValidException;
 import com.foliapp.userservice.useCase.UserUseCase;
 
 import javax.enterprise.context.RequestScoped;
+import java.util.ArrayList;
 
 @RequestScoped
 public class StandardUserUseCase implements UserUseCase {
@@ -13,6 +15,10 @@ public class StandardUserUseCase implements UserUseCase {
         if (user.getName().length() <= 3) {
             throw new NameLengthNotValidException(user.getName());
         }
+
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(Role.USER);
+        user.setRoles(roles);
 
         return user;
     }
