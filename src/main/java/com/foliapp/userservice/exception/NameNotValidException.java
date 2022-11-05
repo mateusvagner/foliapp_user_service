ed6c380e385f12f0b8ea3354f08374b9a3c0e9c8
Mateus Vagner <mateus.vagner@gmail.com>
2022-11-05 14:29:48 -0300

    refactor: remove unused classes

--- src/main/java/com/foliapp/userservice/exception/NameNotValidException.java
+++
@@ -1,7 +0,0 @@
-package com.foliapp.userservice.exception;
-
-public class NameNotValidException extends RuntimeException {
-    public NameNotValidException(String name) {
-        super("The name " + name + "is not valid.");
-    }
-}
